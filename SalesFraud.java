8c6d65d6f75bd6f0b643b1ee794e1c70a22c3ad2
Adarsh Padalia <340943950@gapps.yrds.ca>
2021-05-11 14:06:39 -0400

    Integrated Iza's code and fixed countNums method

--- SalesFraud.java
+++ SalesFraud.java
@@ -12,17 +12,46 @@ import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.data.category.CategoryDataset; 
 import org.jfree.data.category.DefaultCategoryDataset; 
 import javax.swing.JFrame;
+import java.util.Scanner;
+import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
 
  public class SalesFraud {
      public static void main(String[] args) {
-        int[] arr1 = {4, 9, 3, 5, 4, 6, 5, 8, 7, 4, 1, 3, 1, 3, 6, 9, 4, 6};
-        int[] arr2 = countNums(arr1, 9);
-        reportFraud(arr2);
-        CategoryDataset dataset = arrToDataset(arr2, "Counts");
-        createChart("First Digits in Sales Data", "First Digit", "Count", dataset);
-        printArray(arr2);
-     }
+        Scanner reader = new Scanner (System.in); // Initializing Scanner
+        boolean verifyFile = false;
+        String salesFile;
+        do {
+            // Loops if user input is wrong
+            // Prompts user for file
+            System.out.println("\nPlease enter the full file path and name of the file with the sales you would like to verify (EX:C:\\Users\\John\\Desktop\\ICS3U\\sales.csv)");
+            salesFile = reader.nextLine(); // User file input
 
+            // Verifys user input
+            System.out.println("Please confirm that your file input is correct (YES or NO)");
+            String fileConfirmation = reader.nextLine();
+            if(fileConfirmation.equalsIgnoreCase("yes")){
+                verifyFile = true;
+            }
+            else if(fileConfirmation.equalsIgnoreCase("no")){
+                verifyFile = false;
+            }
+            else{
+                System.out.println("Your confirmation input was invalid. We will assume it was NO");
+                verifyFile = false;
+            }
+        } while(!(verifyFile));
+        
+        int[] salesData = getSalesData(salesFile);
+        int[] counts = countNums(salesData, 9);
+        reportFraud(counts);
+        CategoryDataset dataset = arrToDataset(counts, "Counts");
+        createChart("First Digits in Sales Data", "First Digit", "Count", dataset);
+        printArray(counts);
+     }
+     
      /**
       * This method takes in a category dataset and some labels and displays a bar graph in a popup window
       * 
@@ -42,7 +71,7 @@ import javax.swing.JFrame;
         frame.add(new ChartPanel(barGraph));
         frame.setVisible(true);
     }
-    
+
     /**
      * This method takes in an array and a series name and converts it to a category dataset
      * 
@@ -59,7 +88,7 @@ import javax.swing.JFrame;
             
         return dataset;
     }
-        
+    
     /**
      * This method takes in an array and the desired size of the output array and returns the count of
      * each value that appears in that array
@@ -75,7 +104,7 @@ import javax.swing.JFrame;
         int[] counts = new int[arrSize];
         
         for (int num : arr) {
-            counts[num] += 1;
+            counts[num-1] += 1;
         }
 
         return counts;
@@ -96,7 +125,7 @@ import javax.swing.JFrame;
             sum += arr[i];
         }
 
-        double frequency = arr[0]/sum;
+        double frequency = 1.0*arr[0]/sum;
         if (frequency >= 0.29 && frequency <= 0.32) {
             System.out.println("\nIt is likely that fraud didn't occur in this set of sales data.");
         }
@@ -121,4 +150,44 @@ import javax.swing.JFrame;
         }
         System.out.println("\n");
     }
- }
+
+    /**
+     * This method takes in the user inputed file and reads it to find the first digit of every sales number. Then it outputs an array with the first digits
+     * 
+     * @param fileName       The file name and location
+     * @return firstDigits   An array containing the first digit of every sale number
+     */
+    public static int[] getSalesData(String fileName){
+        File salesFile = new File(fileName);  
+        ArrayList<Integer> firstNums = new ArrayList<Integer>(); // Create an array list because we do not know the number of sales
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(salesFile)); // Get the file to read
+            String str = reader.readLine(); // Read the first line to disriard the title (Postal Code,Sales)
+            while(str != null){ // Loop to read every line
+                str = reader.readLine(); // Reading the line
+                
+                if(str != null){
+                    String[] parts = str.split(","); // Seperates the line using the comma
+                    char c = parts[1].charAt(0); // Finds the first character of the second part which is the sales number
+                    int num = Integer.parseInt("" + c); // Converts the character into a integer
+                    firstNums.add(num); // Adds the integer into the array list
+                }
+                
+            }
+            reader.close();
+        }
+        catch(Exception e){ // Incase an error occures, informs the user of the error and quits the program
+            System.out.println("An error has occured please try again");
+            System.out.println(e);
+            System.exit(-1);
+        }
+
+        int[] firstDigits = new int[firstNums.size()]; // Creating an array to covert the array list into it
+
+        for(int i = 0; i<firstNums.size(); i++){
+            firstDigits[i] = firstNums.get(i); // Converting the array list into the array firstDigits
+        }
+
+        return firstDigits; 
+    }
+}
