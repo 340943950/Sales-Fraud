4f3aaf930e98085acdf0364ed7c55a95a0481296
Adarsh Padalia <340943950@gapps.yrds.ca>
2021-05-12 07:50:46 -0400

    Added method that prints results to a csv file (Iza's code)

--- SalesFraud.java
+++ SalesFraud.java
@@ -13,17 +13,16 @@ import org.jfree.data.category.CategoryDataset;
 import org.jfree.data.category.DefaultCategoryDataset; 
 import javax.swing.JFrame;
 import java.util.Scanner;
-import java.io.File;
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.*;
 import java.util.ArrayList;
+import java.text.DecimalFormat;
 
  public class SalesFraud {
      public static void main(String[] args) {
         Scanner reader = new Scanner (System.in); // Initializing Scanner
         boolean verifyFile = false;
         String salesFile;
-        do {
+        do{
             // Loops if user input is wrong
             // Prompts user for file
             System.out.println("\nPlease enter the full file path and name of the file with the sales you would like to verify (EX:C:\\Users\\John\\Desktop\\ICS3U\\sales.csv)");
@@ -47,10 +46,12 @@ import java.util.ArrayList;
         
         int[] salesData = getSalesData(salesFile);
         int[] counts = countNums(salesData, 9);
-        reportFraud(counts);
+        double[] frequencies = getFrequencies(counts); // Array with the percentage/frequency of every digit
+        reportFraud(frequencies);
         CategoryDataset dataset = arrToDataset(counts, "Counts");
         createChart("First Digits in Sales Data", "First Digit", "Count", dataset);
         printArray(counts);
+        printResults("results.csv", frequencies); // Creating the results file
      }
      
      /**
@@ -94,7 +95,7 @@ import java.util.ArrayList;
      * This method takes in an array and the desired size of the output array and returns the count of
      * each value that appears in that array
      * 
-     * This method only works if arr doesn't have any negative values and arrSize is equal to the largest
+     * This method only works if arr doesn't have any negative or 0 values and arrSize is equal to the largest
      * possible value in arr
      * 
      * @param arr       The array in which to count values
@@ -114,20 +115,15 @@ import java.util.ArrayList;
     /**
      * This method checks if the first digit is a one from 29 to 32 percent of the time
      * 
-     * If it does occur from 29 to 32 percent of the time fraud likely didn't occur, otherwise may have occurred
+     * If it does occur from 29 to 32 percent of the time fraud likely didn't occur, otherwise fraud may have occurred
      * 
-     * @param arr   The array with the counts of each first digit
+     * @param arr   The array with the percentage of times each digit occurred
      */
-    public static void reportFraud (int[] arr) {
+    public static void reportFraud (double[] arr) {
         // The first element in the array is the frequency of the first digit occurring
         // According to Benford's Law, if the frequency is between 29% and 32% then fraud didn't occur
-        int sum = 0;
-        for (int i = 0; i < arr.length; i++) {
-            sum += arr[i];
-        }
 
-        double frequency = 1.0*arr[0]/sum;
-        if (frequency >= 0.29 && frequency <= 0.32) {
+        if (arr[0] >= 0.29 && arr[0] <= 0.32) {
             System.out.println("\nIt is likely that fraud didn't occur in this set of sales data.");
         }
         else {
@@ -143,11 +139,11 @@ import java.util.ArrayList;
     public static void printArray (int[] arr) {
         System.out.println("\nFirst Digit Counts:");
         for (int i = 1; i <= 9; i++) {
-            System.out.format("%5s", i);
+            System.out.format("%5s", i);        // Prints out the column headers in table format
         }
         System.out.println("\n  =============================================");
         for (int j = 0; j < arr.length; j++) {
-            System.out.format("%5s", arr[j]);
+            System.out.format("%5s", arr[j]);   // Prints out the frequecies in table format
         }
         System.out.println("\n");
     }
@@ -190,5 +186,56 @@ import java.util.ArrayList;
         }
 
         return firstDigits; 
+    }
+
+    /**
+     * This method takes in the results file name and the int array count. Then it outputs the results file with the digit and its frequency
+     * 
+     * @param fileName    The file results.csv
+     * @param frequencies An array containing the frequency of the digits
+     */
+    public static void printResults(String fileName, double[] frequencies){
+        try{
+            FileWriter myWriter = new FileWriter(fileName); 
+            BufferedWriter bw = new BufferedWriter(myWriter);
+            PrintWriter pw = new PrintWriter(bw);
+            pw.println("Digit,Frequency"); // Prints the title into the file
+            DecimalFormat df = new DecimalFormat("#.#"); // Rounds the frequencies to the tenth decimal place for better display
+            for(int i=1; i<frequencies.length+1; i++){ 
+                pw.println(i +","+ df.format(frequencies[i-1]) + "%"); // Prints every digit and its frequency
+            }
+            pw.flush();
+            pw.close();
+            System.out.println ("Successfully wrote in the file"); // Informs the user of successful completion
+        }
+        catch (Exception e) { // If an error occurs
+            System.out.println("An error has occured please try again");
+            System.out.println(e);
+        }
+    }
+    
+    /**
+     * This method takes in the counts array and converts it into frequencies for each digit
+     * 
+     * @param counts       The array containing the count of each first digit
+     * @return frequencies An array containing the frequency of the digits
+     */
+    public static double[] getFrequencies(int[] counts){
+        // Declairing variables
+        double sum = 0.0;
+        double[] frequencies = new double[counts.length];
+        double freqTemp = 0.0;
+
+        for (int num: counts){
+            sum += num; // Finding the sum of all the counts
+        }
+
+        for(int i = 0; i<counts.length; i++){
+            freqTemp = (counts[i]*100)/sum; // Finding the percentage of each count
+            frequencies[i] = freqTemp; // Placing the frequency into the frequencies array
+        }
+
+        return frequencies;
+
     }
 }
